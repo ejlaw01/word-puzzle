62df07d6775f5c4fccf84c76405a5e97d94bbc66
Ethan Law and Stephen Newkirk <stephen@>
2016-08-16 16:50:55 -0700

    interface will now replace all vowels with dashes

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//business logic
+var i=0;
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+var secretSentence = [];
+var replaceVowels = function(input) {
+  for (i=0; i < input.length; i++) {
+    if (vowels.includes(input[i])) {
+      secretSentence.push("-");  //alternately secretSentence.push(input[i] = "-");
+    } else {
+      secretSentence.push(input[i]);
+    }
+  }
+  var joinedSecretSentence = secretSentence.join("");
+  return joinedSecretSentence;
+}
+
+//user-interface logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#sentence").val().split("");
+    var newSentence = replaceVowels(userInput);
+    $("#output").text(newSentence);
+  });
+});
